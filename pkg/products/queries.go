a6b1ff4d2a1c0e89f0ab4471c6c9b6a7dbbd0b88
henritouchard <henritouchard@gmail.com>
2020-06-05 01:07:05 +0200

    modify misunderstood parameter and explain

--- pkg/products/queries.go
+++ pkg/products/queries.go
@@ -1,14 +1,23 @@
 package products
 
 const (
+	// Note about the OVERLAPS psql function :
+	// there was only 3 days of delay from shipping to rental and 4 days from rental to
+	// cleaning. OVERLAPS will return true if dates overlaps however this
+	//   (DATE '2011-01-28', DATE '2011-02-01') OVERLAPS
+	//   (DATE '2011-02-01', DATE '2011-02-01');
+	// returns false. for our case it means that we could ship the product the same day
+	// it's cleaned, what we can't do since it's back in stock the day after.
+	// That's why we compare OVERLAPS with one day more than delay, both at begining and end of rental
 	queryProductAvailability = `
 	SELECT p.stock - 
 	(
 		SELECT COALESCE(SUM(r.quantity), 0) 
 			FROM rental_order as r 
 			WHERE r.product = p.id
-				AND (r.start_date, r.end_date + INTERVAL '5 DAYS') OVERLAPS ($2::date - INTERVAL '2 DAYS', $3::date)
-				IS TRUE
+				AND (r.start_date - INTERVAL '3 DAYS', r.end_date + INTERVAL '4 DAYS') 
+					OVERLAPS ($2::date - INTERVAL '4 DAYS', $3::date + INTERVAL '5 DAYS')
+					IS TRUE
 	) AS availability
 	FROM product as p WHERE p.id=$1
 	`
@@ -19,8 +28,9 @@ const (
 		SELECT COALESCE(SUM(r.quantity), 0)  
 			FROM rental_order as r 
 			WHERE r.product = p.id
-				AND (r.start_date, r.end_date + INTERVAL '5 DAYS') OVERLAPS ($1::date - INTERVAL '2 DAYS', $2::date) 
-				IS TRUE
+				AND (r.start_date - INTERVAL '3 DAYS', r.end_date + INTERVAL '4 DAYS') 
+					OVERLAPS ($1::date - INTERVAL '4 DAYS', $2::date + INTERVAL '5 DAYS') 
+					IS TRUE
 	) AS availability, p.picture
 		FROM product as p WHERE cstr_category=$3
 	`
@@ -30,7 +40,8 @@ const (
 		SELECT COALESCE(SUM(r.quantity), 0) as total
 		FROM rental_order as r 
 		WHERE r.product = p.id
-		AND (r.start_date, r.end_date + INTERVAL '5 DAYS') OVERLAPS ($3::date - INTERVAL '2 DAYS', $4::date)) 
+		AND (r.start_date - INTERVAL '3 DAYS', r.end_date + INTERVAL '4 DAYS') 
+			OVERLAPS ($3::date - INTERVAL '4 DAYS', $4::date + INTERVAL '5 DAYS')) 
 		AS available FROM product as p WHERE p.id = $1;
 	`
 
@@ -39,8 +50,8 @@ const (
 		SELECT COALESCE(SUM(r.quantity), 0) as total
 		FROM rental_order as r 
 		WHERE r.product = p.id
-		AND (r.start_date, r.end_date + 
-        INTERVAL '5 DAYS') OVERLAPS ($1::date - INTERVAL '2 DAYS', $2::date)) 
+		AND (r.start_date - INTERVAL '3 DAYS', r.end_date + INTERVAL '4 DAYS') 
+			OVERLAPS ($1::date - INTERVAL '4 DAYS', $2::date + INTERVAL '5 DAYS')) 
 		AS available FROM product as p;
 	`
 
